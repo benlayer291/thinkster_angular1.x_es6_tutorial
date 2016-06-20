6957718b13702c63aa06dfc2428d81d8afbfeb1b
benlayer <ben@tuimedia.com>
2016-06-20 17:54:20 +0100

    Implement authentication

---
+++ src/js/auth/auth.controller.js
@@ -0,0 +1,30 @@
+class AuthCtrl {
+  constructor(User, $state) {
+    'ngInject';
+
+    this._User = User;
+    this._$state = $state;
+
+    this.title = $state.current.title;
+    this.authType = $state.current.name.replace('app.', '');
+
+  }
+
+  submitForm () {
+    this.isSubmitting = true;
+
+    this._User.attemptAuth(this.authType, this.formData).then(
+      // Callback for success
+      (res) => {
+        this._$state.go('app.home');
+      },
+      // Callback for error
+      (err) => {
+        this.isSubmitting = false;
+        this.errors = err.data.errors;
+      }
+    );
+  }
+}
+
+export default AuthCtrl;
